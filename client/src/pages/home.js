0ddd8c169d08e030d06880287ec785e344a663dd
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-04-21 14:50:11 -0400

    set up App.js with react-router-dom and created HomePage component in home.js

---
+++ client/src/pages/home.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState, useRef} from 'react';
+
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the home page of our website.</p>
+        </div>
+    )
+}
+
+
+
+
+export default HomePage
