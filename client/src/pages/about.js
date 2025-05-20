b8da294d2bfd36897a34eb8b5fe9454f1cc46ed4
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-05-20 14:26:03 -0400

    created thr about page for testing

---
+++ client/src/pages/about.js
@@ -0,0 +1,13 @@
+// src/pages/about.js
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">About Us</h2>
+      <p>This is the About Page content.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
