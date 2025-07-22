b27221efb09bb06b902af561ff519dafc5c3f98c
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-07-22 14:37:18 -0400

    added wikedLogo to home page centered on page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,8 +2,7 @@ import React, {useEffect, useState, useRef} from 'react';
 import Header from '../components/header.js';
 
 import backgroundImage from '../Images/havasuBridge.jpg';
-
-
+import wickedLogo from '../Images/wickedFabLogo.jpg'
 
 const HomePage = () => {
 
@@ -26,11 +25,13 @@ const HomePage = () => {
                 {/* Optional content centered over background */}
                 <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
                     <h1 className="text-4xl font-bold">Welcome to the Page</h1>
+                    <img src={wickedLogo} alt="Wicked Logo" className="w-48 h-48 object-cover mt-4" />
+
                     <p className="text-lg mt-4">This is some content over the background.</p>
                 </div>
             </section>
 
-            {/* Other page content below */}
+            {/* Other page content below footer? */}
             <section className="bg-white p-6">
                 {/* Your next section or content here */}
             </section>
