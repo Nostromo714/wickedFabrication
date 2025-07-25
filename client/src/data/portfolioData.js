24bda94ecb6d134c4776e8a0b0d967c398273a1b
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-07-25 14:52:07 -0400

    moved portfolioImages to public folder for faster acsess. updated portfolioData with clients pictures

--- client/src/data/portfolioData.js
+++ client/src/data/portfolioData.js
@@ -12,25 +12,144 @@ export const portfolioItems = [
     },
     {
         id: 2,
-        title: "Metal Shop Sign 2",
-        image: "path/to/image2.jpg",
+        title: "Metal Arizona Law In Shop Sign",
+        image: "/portfolioImages/arizonaLawInShop.jpg",
         category: "SIGNS",
         tags: ["metal", "sign", "art"]
     },
     {
         id: 3,
-        title: "Metal Art Piece 1",
-        image: "path/to/image3.jpg",
-        category: "ART",
-        tags: ["metal", "art", "sculpture"]
+        title: "Metal Buller Sign",
+        image: "/portfolioImages/metalBullerSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
     },
     {
         id: 4,
-        title: "Metal Art Piece 2",
-        image: "path/to/image4.jpg",
+        title: "Wood Lake Havasu City Sign",
+        image: "/portfolioImages/WoodLakeHavasuCitySign.webp",
+        category: "SIGNS",
+        tags: ["metal", "art", "sculpture"]
+    },
+    {
+        id: 5,
+        title: "Metal HAlk Art",
+        image: "/portfolioImages/metalHalkArt.jpg",
         category: "ART",
         tags: ["wood", "art", "abstract"]
-    }
+    },
+    {
+        id: 6,
+        title: "Metal Feathers",
+        image: "/portfolioImages/feathers.jpg",
+        category: "ART",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 7,
+        title: "Metal Eagle",
+        image: "/portfolioImages/metalEagle.jpg",
+        category: "ART",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 8,
+        title: "Welcome to BS Ranch Metal Sign",
+        image: "/portfolioImages/welcomeToBsRanchMetal.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 9,
+        title: "Iron Wolf Sign",
+        image: "/portfolioImages/ironWolfSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 10,
+        title: "Metal Dragon Shop Sign",
+        image: "/portfolioImages/metalDragonShop.jpg",
+        category: "ART",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 11,
+        title: "Sunrise Sign",
+        image: "/portfolioImages/sunriseSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 12,
+        title: "Sunrise with Light Sign",
+        image: "/portfolioImages/sunriseWith LightSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 13,
+        title: "Iron Wolf Sign on Brick",
+        image: "/portfolioImages/ironWolfSignOnBrick.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 14,
+        title: "Refuge Private Property Sign",
+        image: "/portfolioImages/refugePrivatePropertySign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 15,
+        title: "Auto Focus Sign",
+        image: "/portfolioImages/autoFocusSing.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 16,
+        title: "Bradley Driving Range Sign",
+        image: "/portfolioImages/bradleyDrivingRangeSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 17,
+        title: "Oil Change Sign",
+        image: "/portfolioImages/oilChangeSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 18,
+        title: "South Gate Sign",
+        image: "/portfolioImages/southGateSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 19,
+        title: "North Gate Sign",
+        image: "/portfolioImages/northGateSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 20,
+        title: "Mulligan's Den Sign",
+        image: "/portfolioImages/mulligansDenSign.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 21,
+        title: "Islander Pavilion Light Sign",
+        image: "/portfolioImages/islanderPavilionLight.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
 ];    
 
 export default portfolioItems;
