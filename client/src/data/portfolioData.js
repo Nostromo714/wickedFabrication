99c3e0da5e2eece687606670cf30bd38ccf37be8
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-07-25 12:46:13 -0400

    created data directory to hold portfolioData directory with the mock CMS data source code. created portfolio gallery logic to organize pictures with tags and catogorys with styles using tailwindsCSS. and imported portfolioGallery to portfolio page

---
+++ client/src/data/portfolioData.js
@@ -0,0 +1,36 @@
+//ths will act as a mock "CMS" data source //
+//src/data/portfolioData.js//
+
+
+export const portfolioItems = [
+    {
+        id: 1,
+        title: "Bogeys and Stogies sports lounge and grill sign",
+        image: "/portfolioImages/bogeys-stogies.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "wood",]
+    },
+    {
+        id: 2,
+        title: "Metal Shop Sign 2",
+        image: "path/to/image2.jpg",
+        category: "SIGNS",
+        tags: ["metal", "sign", "art"]
+    },
+    {
+        id: 3,
+        title: "Metal Art Piece 1",
+        image: "path/to/image3.jpg",
+        category: "ART",
+        tags: ["metal", "art", "sculpture"]
+    },
+    {
+        id: 4,
+        title: "Metal Art Piece 2",
+        image: "path/to/image4.jpg",
+        category: "ART",
+        tags: ["wood", "art", "abstract"]
+    }
+];    
+
+export default portfolioItems;
