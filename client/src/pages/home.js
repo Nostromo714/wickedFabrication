b28c49961b37f311c9545ea7c3a724c9f52046ad
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-09-10 14:01:09 -0400

    updated styles for heroQuotes box, updated notes took out a section and repced with padding top to create the seperation in to What We do section.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -20,22 +20,18 @@ const HomePage = () => {
                 <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
 
                 {/* Header on top of background */}
-                <header className='pb-20'>
+                <header className='pb-2'>
                     <Header />
                 </header>
 
-                {/* Optional content centered over background */}
-                <div className="border-green-300 border-b-4 border-l-4 border-t-4 border-r-4 rounded-lg w-10/12 max-w-4xl mx-auto bg-black bg-opacity-50 text-center p-2 relative z-10">
+                {/* Hero Quotes with box styling and text styling */}
+                <div className=" border-b-4 border-l-4 border-t-4 border-r-4 rounded-lg w-10/12 max-w-4xl mx-auto bg-black bg-opacity-50 text-center p-2 relative z-10">
                     <HeroQuotes />
                 </div>
             </section>
 
-            {/* Other page content below footer? */}
-            <section className="bg-black p-6">
-                {/* Your next section or content here */}
-                
-            
-            <section className="bg-gray-800 text-white p-6">
+            {/* What We Do Content/component */}
+            <section className="bg-gray-800 text-white p-6 border-t-4">
                 <div className="max-w-4xl mx-auto text-center">
                     <img src={transparentLogo} alt="Wicked Fabrication Logo" className="mx-auto mb-4 w-48 h-auto" />
                     <h2 className="text-2xl md:text-3xl font-bold mb-4">What We Do</h2>
@@ -43,8 +39,7 @@ const HomePage = () => {
                     <p className="text-md md:text-lg">Explore our custom fabrication services and see how we can bring your vision to life.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatum dolores assumenda libero nisi tempora facere, quod ipsam. Nesciunt, ullam. Adipisci modi veritatis beatae culpa ipsam repudiandae commodi assumenda soluta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, rem quae eaque fugiat culpa, facere dolor labore odio cumque ex eius dicta cum, tenetur mollitia in ullam unde. Tempore, voluptas.</p>
                     </div>
-                    </section>
-            </section>
+                </section>
         </div>
     );
 };
