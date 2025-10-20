fe7e5ca98250f88e8b7fd193d18dc114a1b43516
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-10-20 11:30:14 -0400

    updated padding and other spacing styles, added a header over the heroQuotes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -26,18 +26,25 @@ const HomePage = () => {
         <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
 
         {/* Transparent logo */}
-        <section className="p-2 relative z-10 flex flex-row text-center pt-4">
+        <section className="p-2 relative z-10 flex flex-row text-center p-8">
           <img src={transparentLogo} alt="Wicked Logo" className="w-48 h-48" />
 
           <div className="flex flex-col text-left pl-4">
             <h1 className="text-white text-2xl sm:text-3xl font-bold drop-shadow-lg pt-10">
               Wicked Fabrication
             </h1>
-            <p className="text-white text-sm sm:text-base italic drop-shadow-lg">
-              Custom Metal Fabrication & Signage
-            </p>
+             <p className="text-white text-sm sm:text-base italic drop-shadow-lg">
+                      Custom Metal Fabrication & Signage
+                    </p>
           </div>
         </section>
+
+       <p className="animated-gradient-text text-white sm:text-4xl md:text-5xl lg:text-6xl italic drop-shadow-lg pb-30 p-10 text-center font-bold">
+  Welcome to Wicked Fabrication — Built to Last. Made to Stand Out.
+</p>
+
+<p>"Welcome to Wicked Fabrication — Built to Last. Made to Stand Out.
+        </p>
 
         {/* Hero Quotes with box styling and text styling */}
         <div className="border-b-4 border-l-4 border-t-4 border-r-4 rounded-lg w-9/12 max-w-4xl mx-auto bg-black bg-opacity-50 text-center p-2 pt-30 relative z-10">
